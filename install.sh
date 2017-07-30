8a8dc7f99c94043713a863b23138e54dd34f93f9
Fonix Hardcastle <me@fonix.online>
2017-07-30 22:15:37 +0100

    Updating with known missing deps

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+echo "Welcome to the Node and NPM Installer!"
+sleep 1
+
+echo "Starting installer in 10 seconds!
+sleep 10
+
 echo "Running APT get"
 sudo apt-get update
 
@@ -10,5 +16,11 @@ sudo apt-get install npm -y
 echo "Installing build-essential and libssl-dev"
 sudo apt-get install build-essential libssl-dev -y
 
+echo "Also adding some plugins that may assist NPM"
+sudo apt-get install libavahi-compat-libdnssd-dev -y
+
+sleep 1
 echo "Done"
+
+
 
