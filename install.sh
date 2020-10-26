da7d84524683351b85a7a7aa568f79ecbc0f6789
mayer <43678009+SodaxoPastel@users.noreply.github.com>
2020-10-25 22:59:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,14 @@
-read -p "Are you on Ubuntu 16.04 (y/n)?" CONT
+read -p "Are you on Ubuntu 18.04 (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
 
-echo "Welcome to the Node and NPM Installer!"
+echo "Welcome to the PM2 Installer!"
 sleep 1
 
 echo "Starting installer in 10 seconds!"
 sleep 10
 
-echo "Running APT get"
-sudo apt-get update
-
-echo "Installing NodeJS"
-sudo apt-get install nodejs -y
-
-echo "Installing NPM"
-sudo apt-get install npm -y
-
-read -p "Do you want me to install some extra bits that might fix certain applications? (y/n)?" CONT
-if [ "$CONT" = "y" ]; then
-echo "Installing build-essential and libssl-dev"
-sudo apt-get install build-essential libssl-dev -y
-else 
-echo "Ok! Moving on"
-fi
-
-echo "Also adding some plugins that may assist NPM"
-sudo apt-get install libavahi-compat-libdnssd-dev -y
-
-echo "Run the Npm updater"
-npm install npm@latest -g
-
-echo "Fix the Node global location"
-sleep 1
-ln -s /usr/bin/nodejs /usr/bin/node
+echo "Installing with NPM"
+sudo npm install pm2@latest -g
 
 sleep 1
 else 
